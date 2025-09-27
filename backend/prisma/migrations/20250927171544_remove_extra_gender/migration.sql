1f9d83982ea72e1dd4ef9412fadb814e18ea32d9
Ayush <ayushkansal303@gmail.com>
2025-09-27 22:46:01 +0530

    fix: gender fix

---
+++ backend/prisma/migrations/20250927171544_remove_extra_gender/migration.sql
@@ -0,0 +1,14 @@
+/*
+  Warnings:
+
+  - The values [OTHER] on the enum `Gender` will be removed. If these variants are still used in the database, this will fail.
+
+*/
+-- AlterEnum
+BEGIN;
+CREATE TYPE "public"."Gender_new" AS ENUM ('MALE', 'FEMALE');
+ALTER TABLE "public"."users" ALTER COLUMN "gender" TYPE "public"."Gender_new" USING ("gender"::text::"public"."Gender_new");
+ALTER TYPE "public"."Gender" RENAME TO "Gender_old";
+ALTER TYPE "public"."Gender_new" RENAME TO "Gender";
+DROP TYPE "public"."Gender_old";
+COMMIT;
